213fb8ea9168ceabb7c60ee026364679b3e3e379
vuyisile <vuyisilemark@gmail.com>
2016-02-13 18:37:50 +0200

    created a function that returns values that are less than the given number.

---
+++ tests/26-Oct-2017/get-less-than-number.js
@@ -0,0 +1,8 @@
+function getLessThanNum(arr,num){
+    var getLess = function(val){
+        return val < num;
+    }
+    return arr.filter(getLess);
+}
+
+console.log(getLessThanNum([1,2,3,4,6,8,9,20,15,18,12],12))
