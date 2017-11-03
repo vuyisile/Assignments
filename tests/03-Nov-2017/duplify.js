f94d1e8e584cbef3aa8f42e034e74e6568082915
vuyisile <vuyisilemark@gmail.com>
2017-11-03 11:22:28 +0200

    done with the duplify prototype

--- tests/03-Nov-2017/duplify.js
+++ tests/03-Nov-2017/duplify.js
@@ -1,3 +1,12 @@
-function duplify(num){
-    return num
-}
+
+
+String.prototype.duplify = function(num){
+        var newArr = [];
+        for(var i = num+1; i > this.length;i--){
+            newArr+=this;
+        }
+        return newArr;
+    }
+
+
+ console.log("c".duplify(5))
