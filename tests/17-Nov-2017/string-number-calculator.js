cb98103557da5f21caf1c46a154484484220ac93
vuyisile <vuyisilemark@gmail.com>
2017-11-17 10:45:14 +0200

    Created new variables: nums, sum and  checkForNum.

--- tests/17-Nov-2017/string-number-calculator.js
+++ tests/17-Nov-2017/string-number-calculator.js
@@ -1,5 +1,13 @@
 function addStringNumbers(strNum){
-    return strNum;
+    var nums = /\d/g;
+    var checkForNum = strNum.match(nums);
+    var sum = 0;
+    for(var i in strNum){
+        if(strNum[i] === "" || " "){
+            sum += 0;
+        }
+    }
+    return sum;
 }
 
 console.log(addStringNumbers(""));
