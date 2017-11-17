ae6013148f203d97682420cf06310cf03b46f008
vuyisile <vuyisilemark@gmail.com>
2017-11-17 11:02:33 +0200

    Now the function returns the sum of numbers in a string if strNum ==='' || ' ' it returns sum = 0, else returns sum of numbers in string

--- tests/17-Nov-2017/string-number-calculator.js
+++ tests/17-Nov-2017/string-number-calculator.js
@@ -2,12 +2,15 @@ function addStringNumbers(strNum){
     var nums = /\d/g;
     var checkForNum = strNum.match(nums);
     var sum = 0;
-    for(var i in strNum){
-        if(strNum[i] === "" || " "){
-            sum += 0;
+    for(var i in checkForNum){
+        if(checkForNum === null){
+            sum+=0;
         }
+       sum += parseInt(checkForNum[i]);
     }
     return sum;
 }
 
+console.log(addStringNumbers("1jkk4kj89 po1.2"));
 console.log(addStringNumbers(""));
+console.log(addStringNumbers("1jkk4kj89 po1.6"));
