0cd1999654895e554ceaa67ca5d340d494ce0477
vuyisile <vuyisilemark@gmail.com>
2017-11-17 12:12:43 +0200

    Now the function returns the sum of all arguments that are string numbers

--- tests/17-Nov-2017/string-number-calculator.js
+++ tests/17-Nov-2017/string-number-calculator.js
@@ -1,16 +1,17 @@
-function addStringNumbers(strNum){
-    var nums = /\d/g;
-    var checkForNum = strNum.match(nums);
+function addStringNumbers(){
+    var strNum = Array.from(arguments); 
+    var nums = /(\d)(\d)|(\d).(\d)|\d/g;
     var sum = 0;
-    for(var i in checkForNum){
-        if(checkForNum === null){
-            sum+=0;
+    for(var i in strNum){
+        if(strNum[i].match(nums) !== null){
+
+            sum += parseFloat(strNum[i]);
         }
-       sum += parseInt(checkForNum[i]);
+      sum += 0;
     }
     return sum;
 }
 
-console.log(addStringNumbers("1jkk4kj89 po1.2"));
-console.log(addStringNumbers(""));
-console.log(addStringNumbers("1jkk4kj89 po1.6"));
+console.log(addStringNumbers("1.2",'3','8','y'));
+console.log(addStringNumbers("","1.2",'3'));
+console.log(addStringNumbers("1",'89'));
