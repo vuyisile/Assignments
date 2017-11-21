198169bebbf0b6349592e063cbef8a2535777ee6
vuyisile <vuyisilemark@gmail.com>
2017-11-21 14:27:14 +0200

    I created a conditional statement that checks if value at 0 it true or false, if it is false shift

--- free-code-camp/drop-it.js
+++ free-code-camp/drop-it.js
@@ -2,6 +2,11 @@ function dropElements(arr, func) {
     var len = arr.length;
     var n = 0;
     while(n < len){
+        if(!func(arr[0])){
+            arr.shift();
+        }else{
+            break;
+        }
         n++;
     }
     return arr;
