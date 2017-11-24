caf9ba87953fc229e45174e5ef716c63fe0d5f1d
vuyisile <vuyisilemark@gmail.com>
2017-11-24 11:11:47 +0200

    the second test passed, which is when we have strNum equals to not more than 1 number.

--- tests/24-Nov-2017/add-string-numbers-1.1.js
+++ tests/24-Nov-2017/add-string-numbers-1.1.js
@@ -2,8 +2,12 @@ function addStringNum(strNum){
     var sum = 0;
     if(strNum.length === 0){
         sum+=0;
+    }else{
+        sum+=parseInt(strNum);
     }
     return sum;
 }
 
 console.log(addStringNum(""));
+
+console.log(addStringNum("1"));
