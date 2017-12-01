63731b9fbf36900e5ddbb8bb7850fcbf635f975c
vuyisile <vuyisilemark@gmail.com>
2017-12-01 11:28:34 +0200

    renamed a file is-existing to string-is-existing

--- tests/01-Dec-2017/is-existing.js
+++
@@ -1,9 +0,0 @@
-function isExisting(string, word) {
-    if (string.indexOf(word) === -1) {
-        return false;
-    }
-    return true;
-}
-
-console.log(isExisting("I was once a child", "child"));
-console.log(isExisting("I was once a child", "kid"));
