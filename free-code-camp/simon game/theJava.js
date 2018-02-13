c080652efd4962e99e9512f04d1a26edcfc8b37d
vuyisile <vuyisilemark@gmail.com>
2018-02-13 16:26:21 +0200

    modified version of my Simon Game

--- free-code-camp/simon game/theJava.js
+++ free-code-camp/simon game/theJava.js
@@ -1,19 +1,33 @@
 var colors = ['red', 'blue', 'yellow', 'green']
 var compCombination = [];
 var playerCombination = [];
-var count = 0;
+var score = 0;
 var i = 1;
+
+
+function switchOn() {
+    var state = document.getElementById('switchON');
+    var state2 = document.getElementById('switchOFF');
+    state.style.background = 'green';
+    state2.style.background = '#FFA000';
+}
+
+function switchOff() {
+    var state = document.getElementById('switchOFF');
+    var state2 = document.getElementById('switchON');
+    state.style.background = 'red';
+    state2.style.background = '#FFA000';
+}
+
 function start() {
-    //debugger;
     var random = Math.floor(Math.random() * colors.length);
 
     if (compCombination.length < i) {
         var currentColor = colors[random];
         compCombination.push(currentColor);
         i += 1;
-        console.log("compCombination", compCombination)
     }
-
+    currentColor = "";
     return compCombination
 }
 
@@ -23,36 +37,45 @@ function playUser(id) {
         if (id === 'red') {
             document.getElementById('red').style.background = '#C62828';
             setTimeout(function () {
-                document.getElementById('red').style.background = 'none';
+                document.getElementById('red').style.background = 'white';
             }, 500);
-            playerCombination.push('#C62828');
+            playerCombination.push('red');
         } else if (id === 'green') {
             document.getElementById('green').style.background = '#4CAF50';
             setTimeout(function () {
-                document.getElementById('green').style.background = 'none';
+                document.getElementById('green').style.background = 'white';
             }, 500);
-            playerCombination.push('#4CAF50');
+            playerCombination.push('green');
         } else if (id === 'blue') {
             document.getElementById('blue').style.background = '#1976D2';
             setTimeout(function () {
-                document.getElementById('blue').style.background = 'none';
+                document.getElementById('blue').style.background = 'white';
             }, 500);
-            playerCombination.push('#1976D2');
+            playerCombination.push('blue');
         } else if (id === 'yellow') {
             document.getElementById('yellow').style.background = '#FFB300';
             setTimeout(function () {
-                document.getElementById('yellow').style.background = 'none';
+                document.getElementById('yellow').style.background = 'white';
             }, 500);
-            playerCombination.push('#FFB300');
+            playerCombination.push('yellow');
         }
     }
-    if (playerCombination.length === compCombination.length) {
-        setTimeout(() => runGame(), 1000);
-       
-        playerCombination = [];
-    }
-    //clearTimeout(runGame(), 3000);
     console.log('playerCombination : ', playerCombination);
+    var checker = checkMatch(compCombination, playerCombination);
+
+    if (playerCombination.length === compCombination.length && checker === true) {
+        setTimeout(() => runGame(), 1000);
+        playerCombination = [];
+        score += 1;
+        document.getElementById('score').innerHTML = score;
+    } else if (playerCombination.length === compCombination.length && checker === false) {
+        playerCombination = [];
+        compCombination = [];
+        document.getElementById('switchON').style.background = '#FFA000';
+        alert("sorry colors don't match, you lose!");
+         document.getElementById('score').innerHTML = '--';
+       
+    }
 
 }
 
@@ -61,27 +84,39 @@ function playUser(id) {
 
 
 function runGame() {
-    compCombination = start();
-    var lightUp = flashUp(compCombination);
-    setTimeout(lightUp, 500);
+    var stateColor = document.getElementById('switchON').style.background;
+    if (stateColor === 'green white repeat scroll 0% 0%') {
+        compCombination = start();
+        flashUp(compCombination);
+    }
 }
-
 
 function flashUp(compCombination) {
     var i = 0;
-    
+
     var interval = setInterval(function () {
         document.getElementById(compCombination[i]).style.background = compCombination[i];
-        
         setTimeout(function () {
-            console.log('working', compCombination[i-1],i);
-            document.getElementById(compCombination[i-1]).style.background = 'none';
+            document.getElementById(compCombination[i - 1]).style.background = 'white';
         }, 500);
-        i ++;
 
+        i++;
+
+        if (i >= compCombination.length) {
+            clearInterval(interval);
+        }
     }, 1000)
-    if (i >= compCombination.length) {
-        clearInterval(interval);
-        i = 0;
+
+
+
+}
+
+function checkMatch(comp, player) {
+    comp = comp.toString();
+    player = player.toString();
+    if (player === comp) {
+        return true;
+    } else {
+        return false;
     }
 }
