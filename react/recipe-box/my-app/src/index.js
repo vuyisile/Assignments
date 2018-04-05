2ab04678b632d77a7e0ca7dd424ae2ee959bac4a
vuyisile <vuyisilemark@gmail.com>
2018-04-05 12:34:28 +0200

    added a handleChange function that works for multiple inputs

--- react/recipe-box/my-app/src/index.js
+++ react/recipe-box/my-app/src/index.js
@@ -2,36 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function createRecipeObj(name,recipe){
-    const reci = document.getElementById('recipe').value;
-    const list = document.getElementById('list').value;
-    console.log({name: reci, recipe: list.split(',')});
-    
-    return {name: reci, recipe: list.split(',')}
 
-}
+
+
+// function storeRecipeObj() {
+//     //var LocalStorage = require('node-localstorage').LocalStorage;
+//     //localStorage = new LocalStorage('./scratch');
+//     var currentObj = createRecipeObj();
+
+//     localStorage.setItem(currentObj.name, JSON.stringify(currentObj));
+//     console.log(localStorage.getItem(currentObj.name));
+
+// }
 class RecipeBox extends React.Component {
     constructor() {
         super();
-        this.state = { data: [{ name: 'Pap', recipe: ['boiling water', 'maze'] }] }
+        this.state = { recipeName: '',recipeList: [], recipes: [{ name: 'Pap', recipe: ['boiling water', 'maze'] }] }
     }
- addRecipe(val){
-    val = createRecipeObj();
-    this.setState({data:this.state.data.push(val)});
-    console.log(this.state.data)
- }
-  
+   
+
+    
+
+    handleChange(e) {
+        let change = {}
+        change[e.target.name] = e.target.value
+        this.setState(change)
+        console.log(this.state)
+      }
+createRecipeObj() {
+var
+}
+
     render() {
         return (
             <div>
                 <table>
                     <thead>
-                        <input id= {'recipe'} type={'text'} placeholder={'Please add name here'} />
+                        <th><input name={'recipeName'}  type='text' placeholder={'Please add name here'} onChange ={this.handleChange.bind(this)}/></th>
                     </thead>
                     <tbody>
-                        <textarea id = {'list'} rows={"4"} cols={"50"} placeholder={'Please add ingrediants here and separate them by commas....example: tomato,onion,olive oil'}></textarea>
-                        <button onClick = {this.addRecipe.bind(this)}>Add Recipe</button>
-          
+                        <textarea name={'recipeList'} rows={4} cols={50} placeholder={'Please add ingrediants here and separate them by commas....example: tomato,onion,olive oil'} onChange ={this.handleChange.bind(this)}></textarea>
+                        {/* <button onClick={this.addRecipe.bind(this)}>Add Recipe</button> */}
+
                     </tbody>
                 </table>
             </div>
