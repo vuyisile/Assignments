330a7bb669ef7df7e91bbfba33c678a6e813d934
vuyisile <vuyisilemark@gmail.com>
2018-04-05 13:18:02 +0200

    created a new function that changes the state of recipes = this.state.recipes by pushing or appending new objects in the existing array

--- react/recipe-box/my-app/src/index.js
+++ react/recipe-box/my-app/src/index.js
@@ -17,32 +17,39 @@ import './index.css';
 class RecipeBox extends React.Component {
     constructor() {
         super();
-        this.state = { recipeName: '',recipeList: [], recipes: [{ name: 'Pap', recipe: ['boiling water', 'maze'] }] }
+        this.state = { recipeName: '', recipeList: [], recipes: [{recipe: 'Pap', listOfItems: ['boiling water', 'maze'] }] };
+        
     }
-   
 
-    
+
+
 
     handleChange(e) {
         let change = {}
         change[e.target.name] = e.target.value
         this.setState(change)
         console.log(this.state)
-      }
-createRecipeObj() {
-var
-}
+    }
+    addNewRecipeObj() {
+        var newRecipeObj = {recipe: this.state.recipeName, listOfItems: this.state.recipeList.split(',')};
+        var newRecipes = this.state.recipes;
+        newRecipes.push(newRecipeObj);
+        this.setState({recipes: newRecipes});
+        console.log('new state:',this.state);
+        this.clearInputBoxes();
+
+    }
 
     render() {
         return (
             <div>
                 <table>
                     <thead>
-                        <th><input name={'recipeName'}  type='text' placeholder={'Please add name here'} onChange ={this.handleChange.bind(this)}/></th>
+                        <th><input name={'recipeName'} refs ={'recipeName'} type='text' placeholder={'Please add name here'} onChange={this.handleChange.bind(this)} /></th>
                     </thead>
                     <tbody>
-                        <textarea name={'recipeList'} rows={4} cols={50} placeholder={'Please add ingrediants here and separate them by commas....example: tomato,onion,olive oil'} onChange ={this.handleChange.bind(this)}></textarea>
-                        {/* <button onClick={this.addRecipe.bind(this)}>Add Recipe</button> */}
+                        <textarea name={'recipeList'} refs={'recipeList'} rows={4} cols={50} placeholder={'Please add ingrediants here and separate them by commas....example: tomato,onion,olive oil'} onChange={this.handleChange.bind(this)}></textarea>
+                        <button onClick={this.addNewRecipeObj.bind(this)}>Add Recipe</button>
 
                     </tbody>
                 </table>
