5563be7ea080fe2b47100b2eae3a63c1ce9324f7
vuyisile <vuyisilemark@gmail.com>
2018-04-05 16:21:25 +0200

    added localstorage

--- react/recipe-box/my-app/src/index.js
+++ react/recipe-box/my-app/src/index.js
@@ -10,15 +10,12 @@ import './index.css';
 //     //localStorage = new LocalStorage('./scratch');
 //     var currentObj = createRecipeObj();
 
-//     localStorage.setItem(currentObj.name, JSON.stringify(currentObj));
-//     console.log(localStorage.getItem(currentObj.name));
-
 // }
 class RecipeBox extends React.Component {
     constructor() {
         super();
-        this.state = { recipeName: '', recipeList: [], recipes: [{recipe: 'Pap', listOfItems: ['boiling water', 'maze'] }] };
-        
+        this.state = { recipeName: '', recipeList: [], recipes: [{ recipe: 'Pap', listOfItems: ['boiling water', 'maze'] }] };
+
     }
 
 
@@ -31,13 +28,29 @@ class RecipeBox extends React.Component {
         console.log(this.state)
     }
     addNewRecipeObj() {
-        var newRecipeObj = {recipe: this.state.recipeName, listOfItems: this.state.recipeList.split(',')};
+        var newRecipeObj = { recipe: this.state.recipeName, listOfItems: this.state.recipeList.split(',') };
         var newRecipes = this.state.recipes;
         newRecipes.push(newRecipeObj);
-        this.setState({recipes: newRecipes});
-        console.log('new state:',this.state);
+        this.setState({ recipes: newRecipes });
+        console.log('new state:', this.state);
         this.clearInputBoxes();
 
+    }
+
+    clearInputBoxes() {
+        this.storeRecipes();
+        this.refs.recipeName.value = '';
+        this.refs.recipeList.value = [];
+        this.setState({ recipeName: '', recipeList: [] });
+        console.log('cleared input boxes')
+    }
+
+    storeRecipes() {
+
+        localStorage.setItem('Recipes', JSON.stringify(this.state.recipes));
+        var item = JSON.parse(localStorage.getItem("Recipes"));
+        console.log('The stored:', item);
+        console.log('Local storage length:', localStorage.length)
     }
 
     render() {
@@ -45,10 +58,10 @@ class RecipeBox extends React.Component {
             <div>
                 <table>
                     <thead>
-                        <th><input name={'recipeName'} refs ={'recipeName'} type='text' placeholder={'Please add name here'} onChange={this.handleChange.bind(this)} /></th>
+                        <th><input name={'recipeName'} ref={'recipeName'} type='text' placeholder={'Please add name here'} onChange={this.handleChange.bind(this)} /></th>
                     </thead>
                     <tbody>
-                        <textarea name={'recipeList'} refs={'recipeList'} rows={4} cols={50} placeholder={'Please add ingrediants here and separate them by commas....example: tomato,onion,olive oil'} onChange={this.handleChange.bind(this)}></textarea>
+                        <textarea name={'recipeList'} ref={'recipeList'} rows={4} cols={50} placeholder={'Please add ingrediants here and separate them by commas....example: tomato,onion,olive oil'} onChange={this.handleChange.bind(this)}></textarea>
                         <button onClick={this.addNewRecipeObj.bind(this)}>Add Recipe</button>
 
                     </tbody>
