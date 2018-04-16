ff25b8c0c44c66a8f17821c1bd268a4fe75bbd60
vuyisile <vuyisilemark@gmail.com>
2018-04-16 08:15:30 +0200

    modified

--- react/recipe-box/my-app/src/index.js
+++ react/recipe-box/my-app/src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import './bootstrap.min.css';
+
+
 
 class RecipeBox extends React.Component {
-    constructor() {
-        super();
-        this.state = { lsFromLocalStorage: [], recipeName: '', recipeList: [], recipes: [{ recipe: '', listOfItems: [] }] };
+    constructor(props) {
+        super(props);
+        this.state = { btnState: true, recipe: '', ingredients: [], recipes: [] };
     }
-
+    currentIndex = 0;
+    value;
     handleChange(e) {
         let change = {}
         change[e.target.name] = e.target.value
@@ -16,49 +20,99 @@ class RecipeBox extends React.Component {
     }
 
     addNewRecipeObj() {
-        var newRecipeObj = { recipe: this.state.recipeName, listOfItems: this.state.recipeList.split(',') };
+        var newRecipeObj = { recipe: this.state.recipe, ingredients: this.state.ingredients.split(',') };
         this.state.recipes.splice(this.state.recipes.length, 0, newRecipeObj)
         this.setState({ recipes: this.state.recipes })
-        console.log("recipes", this.state.recipes)
+
         this.storeRecipes();
         this.clearInputBoxes();
     }
 
     clearInputBoxes() {
-        this.refs.recipeName.value = '';
-        this.refs.recipeList.value = [];
-        this.setState({ recipeName: '', recipeList: [] });
-        console.log('cleared input boxes');
+        this.refs.recipe.value = '';
+        this.refs.ingredients.value = [];
+        this.setState({ recipe: '', ingredients: [] });
+
     }
 
     storeRecipes() {
         localStorage.setItem("allData", JSON.stringify(this.state.recipes));
-        console.log('new state:', this.state);
+
     }
-    
+
     componentDidMount() {
         var oldSate = JSON.parse(localStorage.getItem("allData"));
         if (oldSate) {
             this.setState({ recipes: oldSate })
-            console.log(this.state.recipes);
+
         }
+    }
+
+    changeBtnState() {
+        if (this.state.btnState === false) {
+            this.setState({ btnState: true });
+            console.log("status:", this.state.btnState)
+        } else {
+            this.setState({ btnState: false });
+            console.log("status:", this.state.btnState)
+        }
+    }
+
+    displayIngredients(index) {
+        this.changeBtnState();
+        this.currentIndex = index;
+        var currentRecipe = this.state.recipes[index];
+        var items = currentRecipe.ingredients;
+        // this.setState({currentIndex: index});
+        if (this.state.btnState === true && this.state.recipes.indexOf(currentRecipe) === index) {
+            this.value = items.map(e => (
+                <ol>{e}</ol>));
+        }
+        else if (this.state.recipes.indexOf(currentRecipe) !== index) {
+            this.value = '';
+        }
+        else {
+            this.value = '';
+        }
+
+    }
+
+    editRecipe(index) {
+        this.changeBtnState();
+        var currentRecipe = this.state.recipes[index];
+        var items = currentRecipe.ingredients;
+        console.log('Editing items:', items);
+        console.log('recipe indexed at', index);
     }
 
     render() {
 
         return (
-            <div>
-                <table>
-                    <thead>
-                        <th><input name={'recipeName'} ref={'recipeName'} type='text' placeholder={'Please add name here'} onChange={this.handleChange.bind(this)} /></th>
-                    </thead>
-                    <tbody>
-                        <textarea name={'recipeList'} ref={'recipeList'} rows={4} cols={50} placeholder={'Please add ingrediants here and separate them by commas....example: tomato,onion,olive oil'} onChange={this.handleChange.bind(this)}></textarea>
-                        <button onClick={this.addNewRecipeObj.bind(this)}>Add Recipe</button>
+            <layer className={'ov-lay'}>
+            <div className={'row container'}>
+            
+                <h1 className={'col-md-12 header text-color'}>Recipe Box</h1>
+                <form className={'col-md-6 '} id={"form"}>
+                    <input className={'col-md-12 marginer'} name={'recipe'} ref={'recipe'} type='text' placeholder={'Please add name here'} onChange={this.handleChange.bind(this)} />
+                    <textarea className={'marginer col-md-12 form-control'} name={'ingredients'} ref={'ingredients'} rows={4} cols={50} placeholder={'Please add ingrediants here and separate them by commas....example: tomato,onion,olive oil'} onChange={this.handleChange.bind(this)}></textarea>
+                    <button className={'col-md-12 btn btn-success marginer'} onClick={this.addNewRecipeObj.bind(this)}>Add Recipe</button>
+                </form>
 
-                    </tbody>
-                </table>
+                <div className={'col-md-6 '} id={"btn-panel"} >
+                    {this.state.recipes.map(data => (
+                        <button className={'col-md-12 btn btn-default marginer'} onClick={() => this.displayIngredients(this.state.recipes.indexOf(data))}>{data.recipe}</button>
+                    ))}
+                </div>
+                    <h2 className={'col-md-12 header text-color'}>View Recipe</h2>
+                <div className={'col-md-12 container'} id={"display-panel"}>
+                  
+                        <button className={'col-md-6 btn btn-primary'} onClick={() => this.editRecipe(this.currentIndex)}>Edit</button>
+                        <button className={'col-md-6 btn btn-danger'}>Delete</button>
+                   
+                    <div >{this.value}</div>
+                </div>
             </div>
+            </layer>
         )
     }
 }
