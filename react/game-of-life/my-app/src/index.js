92539977a2907df2159efb46d257cda1d6ee0842
vuyisile <vuyisilemark@gmail.com>
2018-05-10 12:51:33 +0200

    creating a react version of game-of-life

---
+++ react/game-of-life/my-app/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+class GameOfLife extends React.Component{
+    constructor(){
+        super()
+        this.state = {}
+    }
+
+    
+}
+// ========================================
+
+ReactDOM.render(
+    <GameOfLife />,
+    document.getElementById('root')
+);
