a4a8ff846d6c39f502ee62a18ce6e03db8875856
vuyisile <vuyisilemark@gmail.com>
2018-10-02 13:20:16 +0200

    inserting data into the unit_providers table and selecting the data

--- projects/storage-service/db-script.sql
+++ projects/storage-service/db-script.sql
@@ -6,15 +6,15 @@ CREATE TABLE IF NOT EXISTS unit_providers (
 );CREATE TABLE IF NOT EXISTS unit_locations (
     id serial PRIMARY KEY,
     address VARCHAR(255) NOT NULL,   
-    provider_id INT REFERENCES unit_provider(id) NOT NULL
+    provider_id INT REFERENCES unit_providers(id) NOT NULL
 );CREATE TABLE IF NOT EXISTS unit_blocks (
     id serial PRIMARY KEY,
     block_name VARCHAR(255) NOT NULL,
-    location_id INT REFERENCES unit_location(id) NOT NULL
+    location_id INT REFERENCES unit_locations(id) NOT NULL
 );CREATE TABLE IF NOT EXISTS units (
     id serial PRIMARY KEY,
     name VARCHAR(225) NOT NULL,
-    block_id INT REFERENCES unit_block(id),
+    block_id INT REFERENCES unit_blocks(id),
     type_id INT REFERENCES unit_types(id)
 );CREATE TABLE IF NOT EXISTS unit_types (
     id serial PRIMARY KEY,
@@ -28,9 +28,28 @@ CREATE TABLE IF NOT EXISTS unit_providers (
     last_name VARCHAR(100),
     email VARCHAR(100),
     telephone VARCHAR(100),
-    unit_id INT REFERENCES unit(id) NOT NULL
+    unit_id INT REFERENCES units(id) NOT NULL
 );CREATE TABLE IF NOT EXISTS customer_unit_providers (
     id serial PRIMARY KEY,
-    customer_id INT REFERENCES customer(id) NOT NULL,
-    unit_provider_id INT REFERENCES unit_provider(id) NOT NULL
+    customer_id INT REFERENCES customers(id) NOT NULL,
+    unit_provider_id INT REFERENCES unit_providers(id) NOT NULL
 );
+
+
+-- inserting into unit_providers table
+
+INSERT INTO unit_providers (name,telephone_number,email)
+VALUES ('space cube','01129284569','www.spacecube@outlook.com' );
+
+INSERT INTO unit_providers (name,telephone_number,email)
+VALUES ('local units','01124682884','www.localunits@gmail.com');
+
+INSERT INTO unit_providers (name,telephone_number,email)
+VALUES ('vacuume','0123336483','www.vacuume@linkin.com');
+
+
+-- selecting data from unit_providers table
+
+SELECT * FROM unit_providers;
+
+
